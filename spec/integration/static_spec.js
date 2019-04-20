828159150058ffc754137b9e3ed61226ae9fef60
unknown <brianguta2000@yahoo.com>
2019-04-20 15:56:44 -0400

    assignment routing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,20 +1,33 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
+const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
-//#1
+  //#1
   describe("GET /", () => {
 
-//#2
-    it("should return status code 200", (done) => {
+    //#2
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
 
-//#3
+      //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
+        //#4
+        done();
+      });
+    });
 
-//#4
+  });
+
+  describe("GET /about", () => {
+    it("should return status code 200", (done) => {
+      request.get(about, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain('About Us');
         done();
       });
     });
