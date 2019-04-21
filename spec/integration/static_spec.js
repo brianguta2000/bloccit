f1149627add584d26f53b5ed47c972407b4ec04d
unknown <brianguta2000@yahoo.com>
2019-04-21 13:45:45 -0400

    checkpoint redo

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,16 +8,23 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-    it("should return status code 200", (done) => {
+it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+  request.get(base, (err, res, body) => {
+    expect(res.statusCode).toBe(200);
+    expect(body).toContain("Welcome to Bloccit");
+    });
+  })
+  });
+  
+  describe("GET /about", () => {
 
-//#3
+    //#2
+    it("should return status code 200 and have 'About us' in the body of the response", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
-        done();
+        expect(body).toContain("About us");
+        });
+      })
       });
-    });
-
-  });
+  
 });
