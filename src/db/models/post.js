ad95143b83d3785d02436523a5da567cfcc38a1c
unknown <brianguta2000@yahoo.com>
2019-05-08 15:18:40 -0400

    assignment redo comments

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -13,13 +13,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-  
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  }
-    
-
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
@@ -31,24 +28,10 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       onDelete: "CASCADE"
     });
-      Post.hasMany(models.Comment, {
-     foreignKey: "postId",
-     as: "comments"
-   });
-       Post.hasMany(models.Vote, {
-     foreignKey: "postId",
-     as: "votes"
-   });
-      Post.prototype.getPoints = function(){
-
- // #1
-     if(this.votes.length === 0) return 0
-
- // #2
-     return this.votes
-       .map((v) => { return v.value })
-       .reduce((prev, next) => { return prev + next });
-   
+    Post.hasMany(models.Comment, {
+      foreignKey: "postId",
+      as: "comments"
+    });
   };
   return Post;
 };
